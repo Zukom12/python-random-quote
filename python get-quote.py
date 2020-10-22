052a5dbcd44a30c8590cb041f3d404cd952cc433
Zukom12 <73282923+Zukom12@users.noreply.github.com>
2020-10-22 14:46:49 +0100

    Add files via upload

--- python get-quote.py
+++ python get-quote.py
@@ -1,11 +1,19 @@
+'''O  modulo para poder gerar numeros aleatórios num intervalo a definir'''
+
+import random
+
+last = 13
+rnd = random.randint(0, last)
+
 def primary():
   print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  print(quotes[rnd])
 
 if __name__== "__main__":
   primary()
+
